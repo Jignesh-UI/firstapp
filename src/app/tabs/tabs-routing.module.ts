b17f1cde6494dd87425e421429c0cbd368d2da8a
Jignesh Patel <hellojiger@gmail.com>
2022-11-02 00:03:56 +0530

    added Home Module
    added home module

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
@@ -21,14 +25,24 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
+    path:'404',
+    redirectTo:'/tabs/home',
+    pathMatch: 'full'
+  },
+  {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    redirectTo:'/tabs/home',
     pathMatch: 'full'
   }
 ];
